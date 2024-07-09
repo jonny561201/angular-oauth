7de53b2ed028d99300173c3d4cb9f4a14323193e
Jon Graf <jgraf@vermeer.com>
2024-07-09 14:05:23 -0500

    added the logout button

---
+++ src/app/components/logout-button/logout-button.component.ts
@@ -0,0 +1,18 @@
+import {Component, Inject} from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-logout-button',
+  templateUrl: './logout-button.component.html',
+  standalone: true
+})
+export class LogoutButtonComponent {
+  constructor(@Inject(DOCUMENT) public document: Document, private auth: AuthService) {}
+
+  logout() {
+    this.auth.logout({logoutParams: {
+        returnTo: this.document.location.origin
+      }});
+  }
+}
